04394e1a7f5ab156aa35da9146b4643df3b04a72
Muhammad Ghozi <ghozi.mghozi@gmail.com>
2017-06-12 13:48:35 +0700

    intent home activity

--- Day1/app/src/main/java/com/example/muhammadghozi41/day1/HomeActivity.java
+++ Day1/app/src/main/java/com/example/muhammadghozi41/day1/HomeActivity.java
@@ -2,6 +2,7 @@ package com.example.muhammadghozi41.day1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        String nama = getIntent().getStringExtra("name");
+        TextView text = (TextView) findViewById(R.id.textView);
+        text.setText(nama);
     }
 }
--- Day1/app/src/main/java/com/example/muhammadghozi41/day1/MainActivity.java
+++ Day1/app/src/main/java/com/example/muhammadghozi41/day1/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.muhammadghozi41.day1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
-
+import android.content.Context;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,11 +19,19 @@ public class MainActivity extends AppCompatActivity {
         text.setAllCaps(true);
         final EditText input1 = (EditText) findViewById(R.id.input_nama);
         Button semangat = (Button) findViewById(R.id.button_semangat);
+
+        final Intent intent = new Intent(this, HomeActivity.class);
+        final Context ctx = getApplicationContext();
+
+
         semangat.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 text.setAllCaps(false);
                 text.setText("Halo "+input1.getText()+"! Semangat ya!!");
+                intent.putExtra("name", input1.getText().toString());
+
+                ctx.startActivity(intent);
             }
         });
     }

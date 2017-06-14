e757e588fa060357b6d8c349c3a8b6eecf3b2ed2
Muhammad Ghozi <ghozi.mghozi@gmail.com>
2017-06-14 13:26:15 +0700

    day 2 clear

--- Day2/app/src/main/java/com/example/muhammadghozi41/day2/MainActivity.java
+++ Day2/app/src/main/java/com/example/muhammadghozi41/day2/MainActivity.java
@@ -12,6 +12,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.muhammadghozi41.day2.adapter.ListMenuAdapter;
+import com.example.muhammadghozi41.day2.model.ListMenuItem;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,8 +49,34 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-    }
+        ListView listView = (ListView) findViewById(R.id.list_menu);
+        ArrayList<ListMenuItem> listMenuItems = createSampleMenu();
+        final ListMenuAdapter listMenuAdapter = new ListMenuAdapter(MainActivity.this, R.layout.list_menu_layout, listMenuItems);
+        listView.setAdapter(listMenuAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String label = listMenuAdapter.getItem(i).getLabel().toString();
+                Toast.makeText(MainActivity.this, label, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private ArrayList<ListMenuItem> createSampleMenu() {
+        ListMenuItem item1 = new ListMenuItem(1, "http://icons.iconarchive.com/icons/designcontest/ecommerce-business/256/wallet-icon.png", "Check Balance", "check your balance");
+        ListMenuItem item2 = new ListMenuItem(2, "http://icons.iconarchive.com/icons/awicons/vista-artistic/256/coin-add-icon.png", "Top Up", "top up your balance");
+        ListMenuItem item3 = new ListMenuItem(3, "http://icons.iconarchive.com/icons/graphicloads/colorful-long-shadow/256/Cart-icon.png", "Shopping cart", "go to shopping cart");
+        ListMenuItem item4 = new ListMenuItem(4, "http://icons.iconarchive.com/icons/ampeross/qetto-2/256/profile-icon.png", "Profile", "check your profile");
+        ListMenuItem item5 = new ListMenuItem(5, "http://icons.iconarchive.com/icons/ampeross/qetto-2/256/settings-icon.png", "Setting", "go to setting");
+        ArrayList<ListMenuItem> list = new ArrayList<ListMenuItem>();
+        list.add(item1);
+        list.add(item2);
+        list.add(item3);
+        list.add(item4);
+        list.add(item5);
+
+        return list;
+    }
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
